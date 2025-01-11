091b5732f7e1b1d096537db884ad075ff5bfbdd5
Amermm <81873696+Amermm@users.noreply.github.com>
2025-01-11 10:13:38 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies
-apt-get update
-apt-get install -y libasound2-dev portaudio19-dev python3-all-dev python-all-dev libportaudio2 libportaudiocpp0 ffmpeg
-
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
